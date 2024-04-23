0724e182aa7b7c380ead5588fd3583d64002707c
Drew Roberts <github@drewroberts.com>
2024-04-22 20:49:10 -0500

    Change social links to solana

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,8 +6,8 @@
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
                             <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                            <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="https://t.me/WillieClub">Telegram</a>
-                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="https://x.com/WillieClub">Twitter</a>
+                            <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="https://t.me/SolWillie">Telegram</a>
+                                <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="https://x.com/WillieSolana">Twitter</a>
                             </div>
                             <p class="text-sm text-zinc-400 dark:text-zinc-500">© <!-- -->2024<!-- --> WillieClub.org. No rights reserved.</p>
                         </div>
